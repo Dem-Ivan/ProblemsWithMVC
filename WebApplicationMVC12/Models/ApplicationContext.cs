5971b9daea37c92ef0b944bfa0b9e30a0d3907f6
Dem-Ivan <demidenko.vanya@list.ru>
2020-03-22 15:21:55 +0200

    FirstCommit

---
+++ WebApplicationMVC12/Models/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationMVC12.Models
+{
+	public class ApplicationContext : DbContext
+	{
+		public DbSet<Phone> Phones { get; set; }
+		public DbSet<Order> Orders { get; set; }
+
+		public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+		{
+			Database.EnsureCreated();
+		}
+	}
+}
